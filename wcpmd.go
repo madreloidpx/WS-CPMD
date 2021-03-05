8af8d7c2944a6b413ef0c2ae49b55e7ed81c6128
Aliya Miranda <miranda.aliya@gmail.com>
2021-03-05 23:20:07 +0800

    Added file reader

--- wcpmd.go
+++ wcpmd.go
@@ -1,6 +1,34 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
 func main() {
 	nodes := Nodes()
 	nodes.ShowNodes()
+}
+
+func ReadNodeFile(filename string) [][]string {
+	file, err := os.Open(filename)
+	Handle(err)
+	defer file.Close()
+
+	var data [][]string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		fields := strings.Fields(scanner.Text())
+		data = append(data, fields)
+	}
+	err = scanner.Err()
+	Handle(err)
+
+	return data
+}
+
+func Handle(err error) {
+	fmt.Printf(err.Error())
 }
