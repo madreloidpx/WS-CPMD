a5f71de2e56019702cd6647431b3685b88520d5a
Aliya Miranda <miranda.aliya@gmail.com>
2021-03-05 23:15:52 +0800

    removed checker for exist cos program should check before even creating them

--- wq.go
+++ wq.go
@@ -43,49 +43,43 @@ func (wqf *WeakCliqueFactory) CreateWeakClique(nodes []Node) {
 		nodeList = append(nodeList, n.Index)
 	}
 	hexname := HexName(nodeList)
-	_, exist := wqf.WeakCliques[hexname]
-	if exist == false {
-		members := make(map[int]bool)
-		inlink := make(map[int]bool)
-		outlink := make(map[int]bool)
-		for i, n := range nodes {
-			members[n.Index] = true
-			if i == 0 {
-				inlink = n.InLink
-				outlink = n.OutLink
-			} else {
-				inlink = mergeMap(inlink, n.InLink)
-				outlink = mergeMap(outlink, n.OutLink)
-			}
+	members := make(map[int]bool)
+	inlink := make(map[int]bool)
+	outlink := make(map[int]bool)
+	for i, n := range nodes {
+		members[n.Index] = true
+		if i == 0 {
+			inlink = n.InLink
+			outlink = n.OutLink
+		} else {
+			inlink = mergeMap(inlink, n.InLink)
+			outlink = mergeMap(outlink, n.OutLink)
 		}
-		wq := WeakClique{
-			Members: members,
-			InLink:  inlink,
-			OutLink: outlink,
-			Merged:  false,
-		}
-		wqf.WeakCliques[hexname] = wq
-		wqf.Length = wqf.Length + 1
 	}
+	wq := WeakClique{
+		Members: members,
+		InLink:  inlink,
+		OutLink: outlink,
+		Merged:  false,
+	}
+	wqf.WeakCliques[hexname] = wq
+	wqf.Length = wqf.Length + 1
 }
 
 func (wqf *WeakCliqueFactory) MergeCliques(wq1, wq2 WeakClique) {
 	members := mergeMap(wq1.Members, wq2.Members)
 	memlist := ShowMembers(members)
 	hexname := HexName(memlist)
-	_, exist := wqf.WeakCliques[hexname]
-	if exist == false {
-		inlink := mergeMap(wq1.InLink, wq2.InLink)
-		outlink := mergeMap(wq1.OutLink, wq2.OutLink)
-		wq := WeakClique{
-			Members: members,
-			InLink:  inlink,
-			OutLink: outlink,
-			Merged:  false,
-		}
-		wqf.WeakCliques[hexname] = wq
-		wqf.Length = wqf.Length + 1
+	inlink := mergeMap(wq1.InLink, wq2.InLink)
+	outlink := mergeMap(wq1.OutLink, wq2.OutLink)
+	wq := WeakClique{
+		Members: members,
+		InLink:  inlink,
+		OutLink: outlink,
+		Merged:  false,
 	}
+	wqf.WeakCliques[hexname] = wq
+	wqf.Length = wqf.Length + 1
 }
 
 func (wq WeakClique) ShowMembers() []int {
