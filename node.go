7fe32f906fa38649ce2006360efc7a756837a6d6
Aliya Miranda <miranda.aliya@gmail.com>
2021-03-05 17:05:34 +0800

    resetted and created new node and edge function

--- node.go
+++ node.go
@@ -1,97 +1,69 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
-//Node object
+//Node node object
 type Node struct {
-	Index       int
-	Name        string
-	Inlink      []*Edge
-	inlinkSize  int32
-	Outlink     []*Edge
-	outlinkSize int32
+	Name     string
+	Index    int
+	InBinary map[int]bool
+	InLink   map[int]bool
+	Outlink  map[int]bool
 }
 
-//CreateNode create node object
-func CreateNode(index int, name string) Node {
-	n := Node{
-		Index:       index,
-		Name:        name,
-		inlinkSize:  0,
-		outlinkSize: 0,
+type NodeFactory struct {
+	Nodes  map[string]Node
+	Length int
+}
+
+func Nodes() NodeFactory {
+	nodes := NodeFactory{
+		Nodes:  nil,
+		Length: 0,
 	}
+	return nodes
+}
+
+func (nf NodeFactory) CreateNode(name string) Node {
+	n := Node{
+		Name:     name,
+		Index:    nf.Length,
+		InBinary: map[int]bool{nf.Length: true},
+		InLink:   map[int]bool{nf.Length: true},
+		Outlink:  map[int]bool{nf.Length: true},
+	}
+	nf.Nodes[name] = n
+	nf.Length = nf.Length + 1
 	return n
 }
 
-//InsertInlink inserts an edge to incomming link array
-func (n *Node) InsertInlink(edge *Edge) {
-	n.Inlink = append(n.Inlink, edge)
-	n.inlinkSize = n.inlinkSize + 1
+func (nf NodeFactory) ShowNodes() {
+	nodes := reflect.ValueOf(nf.Nodes).MapKeys()
+	fmt.Println(nodes)
 }
 
-//InsertOutlink inserts an edge to outgoing link array
-func (n *Node) InsertOutlink(edge *Edge) {
-	n.Outlink = append(n.Outlink, edge)
-	n.outlinkSize = n.outlinkSize + 1
+func (n Node) AddInLink(in int) {
+	n.InLink[in] = true
 }
 
-//SearchNode searches a node in a node array
-func SearchNode(nodes []*Node, n string) int {
-	for i, node := range nodes {
-		if node.Name == n {
-			return i
-		}
-	}
-	return -1
+func (n Node) AddOutLink(out int) {
+	n.Outlink[out] = true
 }
 
-//Print print a node
-func (n Node) Print() string {
-	var in string
-	var out string
-	for i, edge := range n.Inlink {
-		if i == 0 {
-			in = edge.Name
-		} else {
-			in = in + ", " + edge.Name
-		}
+func (nf NodeFactory) CreateEdge(in, out string) {
+	inNode, exist := nf.Nodes[in]
+	if exist == false {
+		inNode = nf.CreateNode(in)
 	}
-	for i, edge := range n.Outlink {
-		if i == 0 {
-			out = edge.Name
-		} else {
-			out = out + ", " + edge.Name
-		}
+	outNode, exist := nf.Nodes[out]
+	if exist == false {
+		outNode = nf.CreateNode(out)
 	}
-	return fmt.Sprintf("Node(%s, Out:[%s], In:[%s], Index:%d)", n.Name, out, in, n.Index)
-}
-
-//ReduceUniqueNodes reduces a nodelist to unique nodes
-func ReduceUniqueNodes(nodelist []*Node, start int) []*Node {
-	if len(nodelist) == 0 {
-		return nil
-	}
-	if len(nodelist) == start {
-		return nodelist
-	}
-	unique := nodelist[:start+1]
-	for i := start + 1; i < len(nodelist); i++ {
-		if nodelist[start] != nodelist[i] {
-			unique = append(unique, nodelist[i])
-		}
-	}
-	return ReduceUniqueNodes(unique, start+1)
-}
-
-//CompareNodes returns common nodes
-func CompareNodes(n1, n2 []*Node) []*Node {
-	var commonNodes []*Node
-	for _, i := range n1 {
-		for _, j := range n2 {
-			if i == j {
-				commonNodes = append(commonNodes, i)
-			}
-		}
-	}
-	return commonNodes
+	inNode.AddOutLink(outNode.Index)
+	outNode.AddInLink(inNode.Index)
+	nf.Nodes[inNode.Name] = inNode
+	nf.Nodes[outNode.Name] = outNode
 }
--- node_test.go
+++ node_test.go
@@ -4,76 +4,7 @@ import (
 	"testing"
 )
 
-func TestCreateNode(t *testing.T) {
-	var nodeList []Node
-	count := len(nodeList)
-	name := "A"
-	node1 := CreateNode(count, name)
-	if node1.Index != 0 && node1.Name != name {
-		t.Error("CreateNode (", name, ", ", count, ") not created properly. Node is", node1)
-	}
-	nodeList = append(nodeList, node1)
-	count = len(nodeList)
-	name = "B"
-	node2 := CreateNode(count, name)
-	if node2.Index != 1 && node2.Name != name {
-		t.Error("CreateNode (", name, ", ", count, ") not created properly. Node is %s", node2)
-	}
-	nodeList = append(nodeList, node2)
-}
-
-func TestInOutLink(t *testing.T) {
-	var nodeList []Node
-	// A -> B
-	n1 := CreateNode(len(nodeList), "A")
-	nodeList = append(nodeList, n1)
-	n2 := CreateNode(len(nodeList), "B")
-	nodeList = append(nodeList, n2)
-	edge := CreateEdge(0, "AB", &n2, &n1, 1)
-	n1.InsertOutlink(&edge)
-	n2.InsertInlink(&edge)
-	if &edge != n1.Outlink[0] {
-		t.Error("Edge", edge, "not inserted in outlink of node", n1)
-	}
-	if &edge != n2.Inlink[0] {
-		t.Error("Edge", edge, "not inserted in inlink of node", n2)
-	}
-}
-
-func TestSearchNode(t *testing.T) {
-	var nodeList []*Node
-	n1 := CreateNode(len(nodeList), "A")
-	nodeList = append(nodeList, &n1)
-	n2 := CreateNode(len(nodeList), "B")
-	nodeList = append(nodeList, &n2)
-	pos := SearchNode(nodeList, "B")
-	if pos == -1 {
-		t.Error("Node", n2, "was not found")
-	}
-	if pos != 1 {
-		t.Error("Wrong index. Index", pos, "is found when searching", n2)
-	}
-	pos = SearchNode(nodeList, "C")
-	if pos != -1 {
-		t.Error("Found a node when it should not exist. Index is", pos, "for node", nodeList[pos])
-	}
-}
-
-func TestReduceUniqueNodes(t *testing.T) {
-	var nodeList []*Node
-	n1 := CreateNode(len(nodeList), "A")
-	nodeList = append(nodeList, &n1)
-	nodeList = append(nodeList, &n1)
-	n2 := CreateNode(len(nodeList), "B")
-	nodeList = append(nodeList, &n2)
-	nodeList = append(nodeList, &n1)
-	nodeList = append(nodeList, &n2)
-	finalList := []*Node{&n1, &n2}
-
-	result := ReduceUniqueNodes(nodeList, 0)
-	for i, node := range result {
-		if finalList[i] != node {
-			t.Error("Unable to reduce nodelist", finalList, result)
-		}
-	}
+func TestCreateFactory(t *testing.T) {
+	nodes := Nodes()
+	nodes.ShowNodes()
 }
